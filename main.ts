b8d86ebd0e94e4b30dd0d6554402be7df1a26159
Lsomnia <85993656+Lsomnia@users.noreply.github.com>
2025-09-16 09:29:06 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,22 @@
+input.onGesture(Gesture.Shake, function () {
+    basic.clearScreen()
+    randomNumber = randint(0, 4)
+    if (randomNumber == 2) {
+        basic.showString("Sure, whatever")
+    } else if (randomNumber == 1) {
+        basic.showString("Absolutely not")
+    } else if (randomNumber == 3) {
+        basic.showString("404 Answer not found")
+    } else if (randomNumber == 4) {
+        basic.showString("Ask later")
+    } else {
+        basic.showString("I dunno")
+    }
+    basic.showNumber(8)
+})
+let randomNumber = 0
+basic.showString("Ask away!")
+basic.showNumber(8)
+basic.forever(function () {
+	
+})
